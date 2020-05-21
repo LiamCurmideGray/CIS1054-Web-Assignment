949c43d1b2a5dc32c891b9820f19528ea7e58a76
schweiny-08 <40473641+schweiny-08@users.noreply.github.com>
2020-05-21 17:21:26 +0200

    Login pass hash (#27)
    * Test again
    
    * Passwords are now verified through hashes
    
    Co-authored-by: LiamCurmideGray <45282827+LiamCurmideGray@users.noreply.github.com>

--- phpFunctions/loginUser.php
+++ phpFunctions/loginUser.php
@@ -7,19 +7,37 @@
     if(!empty($email) || !empty($password)){
 
         require_once('connectToDB.php');
+        require_once('utils.php');
 
         $conn = connectionDb();
 
-        $sql = "select * from user where Email='$email' and UsrPassword='$pswd' limit 1";
+        $sql = "SELECT UsrPassword FROM user WHERE Email=?";
 
-        $result = mysqli_query($conn, $sql);
+        // $dbPass = mysqli_query($conn, $sql);
+        // $dbPassArr = mysqli_fetch_array($dbPass);
+        // $dbPassStr = implode($dbPassArr);
 
-        if(mysqli_num_rows($result)==1){            
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $valueOfUsrPass = $result->fetch_object();
+
+        $dbPassStr = $valueOfUsrPass->UsrPassword;
+
+        // echo gettype($pswd);
+        // echo gettype($dbPassStr);       
+        
+        // echo password_verify($pswd, $dbPassStr) ? 'true' : 'false';
+        
+        // if(mysqli_num_rows($dbPass)==1){
+        if(password_verify($pswd, $dbPassStr)){            
             header('Location: ../index.php');
             exit;
         }else{
             $_SESSION['result'] = '<script>document.getElementById("loginError").style.opacity = "1";</script>';
             header('Location: ../login.php');    
+            exit;
         }
 
     }else{
