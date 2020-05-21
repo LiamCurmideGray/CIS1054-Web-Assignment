aec0c77c1b9f4ab50d9029e5de19645501048424
LiamCurmideGray <45282827+LiamCurmideGray@users.noreply.github.com>
2020-05-21 22:28:41 +0200

    This file not needed

--- includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "web assignment db";
-
-$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
