6a8833100643335875fa889035ea797154455fef
stefsche <58699420+stefsche@users.noreply.github.com>
2020-06-11 08:05:33 +0200

    Added server-side data validation (#45)

--- emailFunctions/bookingEmail.php
+++ emailFunctions/bookingEmail.php
@@ -19,12 +19,15 @@ $to = "chummbuket@gmail.com";
 $headers = "From: $email_from ";
 $headers .= "Reply to:  $customer_email";
 
-if (mail($to,$email_subject,$email_body,$headers)){
-echo "<script type='text/javascript'> alert('Your booking request has been sent!'); window.location.href = 'http://localhost/CIS1054-Web-Assignment/contact.php'; </script>";
+if (!empty($first_name) && !empty($last_name) && !empty($contact_number) && !empty($customer_email) && $partySize > 0 && filter_var($customer_email, FILTER_VALIDATE_EMAIL)) {
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "<script type='text/javascript'> alert('Your booking request has been sent!'); window.location.href = 'http://localhost/CIS1054-Web-Assignment/contact.php'; </script>";
+    } else {
+        echo "<script type='text/javascript'> alert('Email sending failed!'); window.location.href = 'http://localhost/CIS1054-Web-Assignment/contact.php'; </script>";
+        exit;
+    }
 }
-
 else {
-    echo "<script type='text/javascript'> alert('Email sending failed!'); window.location.href = 'http://localhost/CIS1054-Web-Assignment/contact.php'; </script>";
+    echo "<script type='text/javascript'> alert('Email sending failed! Check all fields.'); window.location.href = 'http://localhost/CIS1054-Web-Assignment/contact.php'; </script>";
     exit;
 }
-?>
